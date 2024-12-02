fd55087f5951f8ae57dd93a2671b985e76fa05eb
surya.adi44 <kmsurya.adi44@gmail.com>
2024-12-03 05:06:20 +0800

    feat: add seeder

---
+++ src/main/resources/db/seed/V0_1_1__seed_user.sql
@@ -0,0 +1,6 @@
+INSERT INTO
+    auth.users (id, name, email, email_verified_at, password, remember_token, created_at, updated_at)
+VALUES
+    ('01938917-1ee8-7584-96b7-0a21a03ce52d', 'User 1', 'user1@webtech.id', NULL, '$2a$10$4sOlHOUt4RJ1wP0crAXBJ.zLKc5vOnNhQSHWJsNfrDXKn5DIo2CYC', NULL, '2024-12-02 20:36:54.134991 +00:00', '2024-12-02 20:36:54.134991 +00:00'),
+    ('01938917-a7e9-7954-b3d9-00fb72914ed6', 'User 2', 'user2@webtech.id', NULL, '$2a$10$hJI84Gp7b10mhjbTXCP4ieynX2l54rkFE4vcKaOs.nqwq6XQNLRv.', NULL, '2024-12-02 20:37:29.193321 +00:00', '2024-12-02 20:37:29.193321 +00:00'),
+    ('01938917-e940-72e7-b356-26402c97bd24', 'User 3', 'user3@worldskills.org', NULL, '$2a$10$XMnYHwCPRV5EoHM1JyXi1eZk8ZEhpqC48bKrMXmKsG7CuY7I7z1Km', NULL, '2024-12-02 20:37:45.920582 +00:00', '2024-12-02 20:37:45.920582 +00:00');
